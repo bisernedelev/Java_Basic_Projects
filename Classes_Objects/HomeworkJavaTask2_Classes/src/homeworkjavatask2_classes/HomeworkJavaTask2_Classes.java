b6a4e248da6e974993ffe1d20f6e1a897d8ea050
biserPC <biserPC@biser>
2018-11-19 15:25:18 +0200

    Lecture 5 Tasks - Classes and Objects

---
+++ Classes_Objects/HomeworkJavaTask2_Classes/src/homeworkjavatask2_classes/HomeworkJavaTask2_Classes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homeworkjavatask2_classes;
+
+/**
+ *
+ * @author biserPC
+ */
+public class HomeworkJavaTask2_Classes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Person person = new Person("Biser",28);
+        person.getDescription();
+    }
+
+}
