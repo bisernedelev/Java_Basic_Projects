c765fad0638ef87328919a02bcb9e2131eaaa1fa
Biser Nedelev <bibo0088@abv.bg>
2019-05-29 16:56:20 +0300

    Update person_characteristics_person.sql

--- Database/person_characteristics_person.sql
+++ Database/person_characteristics_person.sql
@@ -15,10 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `person`
---
-
 DROP TABLE IF EXISTS `person`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
